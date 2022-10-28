6a1a813e2cdcc16c58ca9ed5f836e2db4f504796
badrsaeed <55393290+badrsaeed@users.noreply.github.com>
2022-10-28 15:15:37 +0200

    add the main form to the program

---
+++ Sells_Management/Sells_Management/PL/main_form.Designer.cs
@@ -0,0 +1,238 @@
+﻿
+namespace Sells_Management.PL
+{
+    partial class main_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ملفToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.تسجيلالدخولToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.انشاءنسخةاحتياطيةToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.استعادةنسخةمحفوظةToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.تسجيلالخروجToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.المنتجاتToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.اضافةمنتجToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.إدارةالمنتجاتToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.اضافةصنفجديدToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ادارةالأصنافToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.العملاءToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.اضافةعميلجديدToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ادارةالعملاءToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.اضافةToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ادارةالمبيعاتToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.المستخدمونToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.اضافةمستخدمجديدToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ادارةالمستخدمونToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ملفToolStripMenuItem,
+            this.المنتجاتToolStripMenuItem,
+            this.العملاءToolStripMenuItem,
+            this.المستخدمونToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ملفToolStripMenuItem
+            // 
+            this.ملفToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.تسجيلالدخولToolStripMenuItem,
+            this.انشاءنسخةاحتياطيةToolStripMenuItem,
+            this.استعادةنسخةمحفوظةToolStripMenuItem,
+            this.تسجيلالخروجToolStripMenuItem});
+            this.ملفToolStripMenuItem.Name = "ملفToolStripMenuItem";
+            this.ملفToolStripMenuItem.Size = new System.Drawing.Size(52, 24);
+            this.ملفToolStripMenuItem.Text = "ملف";
+            // 
+            // تسجيلالدخولToolStripMenuItem
+            // 
+            this.تسجيلالدخولToolStripMenuItem.Name = "تسجيلالدخولToolStripMenuItem";
+            this.تسجيلالدخولToolStripMenuItem.Size = new System.Drawing.Size(241, 26);
+            this.تسجيلالدخولToolStripMenuItem.Text = "تسجيل الدخول";
+            // 
+            // انشاءنسخةاحتياطيةToolStripMenuItem
+            // 
+            this.انشاءنسخةاحتياطيةToolStripMenuItem.Name = "انشاءنسخةاحتياطيةToolStripMenuItem";
+            this.انشاءنسخةاحتياطيةToolStripMenuItem.Size = new System.Drawing.Size(241, 26);
+            this.انشاءنسخةاحتياطيةToolStripMenuItem.Text = "انشاء نسخة احتياطية";
+            // 
+            // استعادةنسخةمحفوظةToolStripMenuItem
+            // 
+            this.استعادةنسخةمحفوظةToolStripMenuItem.Name = "استعادةنسخةمحفوظةToolStripMenuItem";
+            this.استعادةنسخةمحفوظةToolStripMenuItem.Size = new System.Drawing.Size(241, 26);
+            this.استعادةنسخةمحفوظةToolStripMenuItem.Text = "استعادة نسخة محفوظة";
+            // 
+            // تسجيلالخروجToolStripMenuItem
+            // 
+            this.تسجيلالخروجToolStripMenuItem.Name = "تسجيلالخروجToolStripMenuItem";
+            this.تسجيلالخروجToolStripMenuItem.Size = new System.Drawing.Size(241, 26);
+            this.تسجيلالخروجToolStripMenuItem.Text = "تسجيل الخروج";
+            // 
+            // المنتجاتToolStripMenuItem
+            // 
+            this.المنتجاتToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.اضافةمنتجToolStripMenuItem,
+            this.إدارةالمنتجاتToolStripMenuItem,
+            this.اضافةصنفجديدToolStripMenuItem,
+            this.ادارةالأصنافToolStripMenuItem});
+            this.المنتجاتToolStripMenuItem.Name = "المنتجاتToolStripMenuItem";
+            this.المنتجاتToolStripMenuItem.Size = new System.Drawing.Size(78, 24);
+            this.المنتجاتToolStripMenuItem.Text = "المنتجات";
+            this.المنتجاتToolStripMenuItem.Click += new System.EventHandler(this.المنتجاتToolStripMenuItem_Click);
+            // 
+            // اضافةمنتجToolStripMenuItem
+            // 
+            this.اضافةمنتجToolStripMenuItem.Name = "اضافةمنتجToolStripMenuItem";
+            this.اضافةمنتجToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.اضافةمنتجToolStripMenuItem.Text = "اضافة منتج";
+            // 
+            // إدارةالمنتجاتToolStripMenuItem
+            // 
+            this.إدارةالمنتجاتToolStripMenuItem.Name = "إدارةالمنتجاتToolStripMenuItem";
+            this.إدارةالمنتجاتToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.إدارةالمنتجاتToolStripMenuItem.Text = "إدارة المنتجات";
+            // 
+            // اضافةصنفجديدToolStripMenuItem
+            // 
+            this.اضافةصنفجديدToolStripMenuItem.Name = "اضافةصنفجديدToolStripMenuItem";
+            this.اضافةصنفجديدToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.اضافةصنفجديدToolStripMenuItem.Text = "اضافة صنف جديد";
+            // 
+            // ادارةالأصنافToolStripMenuItem
+            // 
+            this.ادارةالأصنافToolStripMenuItem.Name = "ادارةالأصنافToolStripMenuItem";
+            this.ادارةالأصنافToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.ادارةالأصنافToolStripMenuItem.Text = "ادارة الأصناف";
+            // 
+            // العملاءToolStripMenuItem
+            // 
+            this.العملاءToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.اضافةعميلجديدToolStripMenuItem,
+            this.ادارةالعملاءToolStripMenuItem,
+            this.اضافةToolStripMenuItem,
+            this.ادارةالمبيعاتToolStripMenuItem});
+            this.العملاءToolStripMenuItem.Name = "العملاءToolStripMenuItem";
+            this.العملاءToolStripMenuItem.Size = new System.Drawing.Size(65, 24);
+            this.العملاءToolStripMenuItem.Text = "العملاء";
+            // 
+            // اضافةعميلجديدToolStripMenuItem
+            // 
+            this.اضافةعميلجديدToolStripMenuItem.Name = "اضافةعميلجديدToolStripMenuItem";
+            this.اضافةعميلجديدToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.اضافةعميلجديدToolStripMenuItem.Text = "اضافة عميل جديد";
+            // 
+            // ادارةالعملاءToolStripMenuItem
+            // 
+            this.ادارةالعملاءToolStripMenuItem.Name = "ادارةالعملاءToolStripMenuItem";
+            this.ادارةالعملاءToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.ادارةالعملاءToolStripMenuItem.Text = "ادارة العملاء";
+            // 
+            // اضافةToolStripMenuItem
+            // 
+            this.اضافةToolStripMenuItem.Name = "اضافةToolStripMenuItem";
+            this.اضافةToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.اضافةToolStripMenuItem.Text = "اضافة بيع جديد";
+            // 
+            // ادارةالمبيعاتToolStripMenuItem
+            // 
+            this.ادارةالمبيعاتToolStripMenuItem.Name = "ادارةالمبيعاتToolStripMenuItem";
+            this.ادارةالمبيعاتToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.ادارةالمبيعاتToolStripMenuItem.Text = "ادارة المبيعات";
+            // 
+            // المستخدمونToolStripMenuItem
+            // 
+            this.المستخدمونToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.اضافةمستخدمجديدToolStripMenuItem,
+            this.ادارةالمستخدمونToolStripMenuItem});
+            this.المستخدمونToolStripMenuItem.Name = "المستخدمونToolStripMenuItem";
+            this.المستخدمونToolStripMenuItem.Size = new System.Drawing.Size(103, 24);
+            this.المستخدمونToolStripMenuItem.Text = "المستخدمون";
+            // 
+            // اضافةمستخدمجديدToolStripMenuItem
+            // 
+            this.اضافةمستخدمجديدToolStripMenuItem.Name = "اضافةمستخدمجديدToolStripMenuItem";
+            this.اضافةمستخدمجديدToolStripMenuItem.Size = new System.Drawing.Size(226, 26);
+            this.اضافةمستخدمجديدToolStripMenuItem.Text = "اضافة مستخدم جديد ";
+            // 
+            // ادارةالمستخدمونToolStripMenuItem
+            // 
+            this.ادارةالمستخدمونToolStripMenuItem.Name = "ادارةالمستخدمونToolStripMenuItem";
+            this.ادارةالمستخدمونToolStripMenuItem.Size = new System.Drawing.Size(226, 26);
+            this.ادارةالمستخدمونToolStripMenuItem.Text = "ادارة المستخدمون";
+            // 
+            // main_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "main_form";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "إدارة المبيعات (نسخة تجريبية)";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ملفToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem تسجيلالدخولToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem انشاءنسخةاحتياطيةToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem استعادةنسخةمحفوظةToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem تسجيلالخروجToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem المنتجاتToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem اضافةمنتجToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem إدارةالمنتجاتToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem اضافةصنفجديدToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ادارةالأصنافToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem العملاءToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem اضافةعميلجديدToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ادارةالعملاءToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem اضافةToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ادارةالمبيعاتToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem المستخدمونToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem اضافةمستخدمجديدToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ادارةالمستخدمونToolStripMenuItem;
+    }
+}
---
+++ Sells_Management/Sells_Management/PL/main_form.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sells_Management.PL
+{
+    public partial class main_form : Form
+    {
+        public main_form()
+        {
+            InitializeComponent();
+        }
+
+        private void المنتجاتToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
